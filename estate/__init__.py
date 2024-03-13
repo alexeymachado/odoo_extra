d85485ebe94f97c804126a334d73ce686a6e253f
alexeymachado <alexey.mosqueda@gmail.com>
2024-03-13 01:08:39 -0400

    Borre el proyecto de prueba

--- estate/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# imported here to avoid dependency cycle issues
-# pylint: disable=wrong-import-position
-from . import controllers
-from . import models
-from . import wizards
--- estate/models/estate_property_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class EstatePropertyType(models.Model):
-    _name = 'estate.property.type'
-    _description = 'Tipos de Propiedad'
-    name = fields.Char(required=True) 
-    property_ids = fields.One2many(comodel_name='estate.property', inverse_name='type_id')   
