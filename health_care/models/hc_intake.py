31ac4646d7edf692464a6879a6d435cda1e5cea5
alexeymachado <alexey.mosqueda@gmail.com>
2024-03-16 20:36:07 -0400

    20240316

--- health_care/models/hc_intake.py
+++ health_care/models/hc_intake.py
@@ -4,15 +4,11 @@ from odoo import _, api, fields, models
 class HCIntake(models.Model):
     _name = 'hc.intake'
     _description = 'Intake'
-    
-    state = fields.Selection(selection=[('Inicio', 'Inicio'), ('Aprobada', 'Aprobada'), ('Cancelada', 'Cancelada'), ('Terminada', 'Terminada')], default='Inicio')
+
+    state = fields.Selection(selection=[('Inicio', 'Inicio'), ('Progreaso', 'Progreso'), ('Cancelada', 'Cancelada'), ('Aprobada', 'Aprobada'), ('Terminada', 'Terminada')], default='Inicio')
     patient_id = fields.Many2one(comodel_name='hc.patient')
     start_date = fields.Date()
     end_date = fields.Date()
     visit_ids = fields.One2many(comodel_name='hc.visit', inverse_name='intake_id')
     insurance_aproved = fields.Boolean()
     contacts_ids = fields.Many2many(comodel_name='hc.contact')
-
-    
-    
-
--- health_care/models/hc_visit.py
+++ health_care/models/hc_visit.py
@@ -1,13 +1,11 @@
 from odoo import _, api, fields, models
 
+
 class HCVisit(models.Model):
     _name = 'hc.visit'
     _description = 'Visit'
     date = fields.Date()
     type_id = fields.Many2one(comodel_name='hc.visit.type')
-    intake_id = fields.Many2one(comodel_name ='hc.intake')
-    nurse_id = fields.Many2one(comodel_name ='hc.nurse')
+    intake_id = fields.Many2one(comodel_name='hc.intake')
+    nurse_id = fields.Many2one(comodel_name='hc.nurse')
     resume = fields.Text()
-
-
-
